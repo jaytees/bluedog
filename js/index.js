dc07d34d4d3577c7a364d687bf412804cb92f163
Jordan Trickett <jordan1trickett@gmail.com>
2021-03-22 21:25:50 +0000

    change js syntax

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
-import { createToggle } from './toggle.js';
+import Toggle from './toggle.js';
 document.addEventListener('DOMContentLoaded', () => {
   // sets up the event listener and functionality
-  createToggle({
+  // createToggle({
+  //   toggleSwitchClass: '.toggle-switch',
+  //   contentContainerClass: '.info'
+  // });
+  const infoToggle = new Toggle({
+    elementModificationFlag: 'lightMode',
+    switchModificationFlag: 'active',
+    currentMode: 'darkMode',
+    modes: {
+      darkMode: 'lightMode',
+      lightMode: 'darkMode'
+    },
+    contentTitleClassName: '.info__content-title--',
+    contentTextClassName: '.info__content-text',
+    switchTextClassName: '.toggle-switch__text',
     toggleSwitchClass: '.toggle-switch',
     contentContainerClass: '.info'
   });
+  infoToggle.createEventListener();
+
   // makes the nav bar appear when user begins to scroll up
   window.onscroll = () => {
     const scrollY = window.pageYOffset || document.documentElement.scrollTop;
--- js/toggle.js
+++ js/toggle.js
@@ -1,11 +1,22 @@
-const toggle = {
-  currentMode: 'darkMode',
-  // opposite values to alternate states
-  modes: {
-    darkMode: 'lightMode',
-    lightMode: 'darkMode'
-  },
-  contentStates: {
+function Toggle({
+  elementModificationFlag,
+  switchModificationFlag,
+  currentMode,
+  modes,
+  contentTitleClassName,
+  contentTextClassName,
+  switchTextClassName,
+  toggleSwitchClass,
+  contentContainerClass
+}) {
+  this.elementModificationFlag = elementModificationFlag;
+  this.switchModificationFlag = switchModificationFlag;
+  this.currentMode = currentMode;
+  this.modes = modes;
+  this.contentTitleClassName = contentTitleClassName;
+  this.contentTextClassName = contentTextClassName;
+  this.switchTextClassName = switchTextClassName;
+  this.contentStates = {
     lightMode: {
       text:
         'As we shed light on the intricacies of psoriasis pathogenesis, we now know that other proinflammatory cytokines supplement the IL-17/23 axis and charge the immune cells in circulation.',
@@ -16,34 +27,37 @@ const toggle = {
         'The complex immunopathogenic mechanisms driving psoriasis involve interactions between keratinocytes and immunecells, overloading the cycle of inflammation that keeps patients plugged into their psoriasis – bound by skin plaques causing clinical symptoms of pain, itching, and burning sensations.',
       button: 'FLIP THE SWITCH'
     }
-  },
-  // handles different stages of the toggle
-  toggleState() {
-    const newMode = this.modes[this.currentMode];
-    this.toggleTitleDisplayProperty(newMode);
-    this.changeTextContent({ className: '.info__content-text', newText: this.contentStates[newMode].text });
-    this.changeTextContent({ className: '.toggle-switch__text', newText: this.contentStates[newMode].button });
-    this.currentMode = newMode;
-  },
-  // set current display none && new to inline
-  toggleTitleDisplayProperty(newMode) {
-    document.querySelector(`.info__content-title--${this.currentMode}`).style.display = 'none';
-    document.querySelector(`.info__content-title--${newMode}`).style.display = 'inline-block';
-  },
-  changeTextContent({ className, newText }) {
-    document.querySelector(className).textContent = newText;
-  }
-};
+  };
+  this.toggleSwitch = document.querySelector(toggleSwitchClass);
+  this.elementToToggle = document.querySelector(contentContainerClass);
+}
 
-// sets up the event listner and triggers the functionality
-// called on DOM load
-export const createToggle = ({ toggleSwitchClass, contentContainerClass }) => {
-  const toggleSwitch = document.querySelector(toggleSwitchClass);
-  const elementToToggle = document.querySelector(contentContainerClass);
-  toggleSwitch.addEventListener('click', () => {
+Toggle.prototype.createEventListener = function () {
+  this.toggleSwitch.addEventListener('click', () => {
     // returns true if it turns light mode on
-    elementToToggle.classList.toggle('lightMode');
-    toggleSwitch.classList.toggle('active');
-    toggle.toggleState();
+    this.elementToToggle.classList.toggle(this.elementModificationFlag);
+    this.toggleSwitch.classList.toggle(this.switchModificationFlag);
+    this.updateUi();
   });
 };
+
+// handles different stages of the toggle
+Toggle.prototype.updateUi = function () {
+  const newMode = this.modes[this.currentMode];
+  this.toggleTitleDisplayProperty(newMode);
+  this.changeTextContent({ className: this.contentTextClassName, newText: this.contentStates[newMode].text });
+  this.changeTextContent({ className: this.switchTextClassName, newText: this.contentStates[newMode].button });
+  this.currentMode = newMode;
+};
+
+// set current display none && new to inline
+Toggle.prototype.toggleTitleDisplayProperty = function (newMode) {
+  document.querySelector(`${this.contentTitleClassName}${this.currentMode}`).style.display = 'none';
+  document.querySelector(`${this.contentTitleClassName}${newMode}`).style.display = 'inline-block';
+};
+
+Toggle.prototype.changeTextContent = function ({ className, newText }) {
+  document.querySelector(className).textContent = newText;
+};
+
+export default Toggle;
