17e282d9f2d287088acea4bb6a9786eff33fc55c
Jordan Trickett <jordan1trickett@gmail.com>
2021-03-22 12:08:52 +0000

    js refactor to oop

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 console.log('Hello Bluedog');
 
-document.addEventListener('DOMContentLoaded', () => {
-  const contentStates = {
+const toggle = {
+  currentMode: 'lightMode',
+  // opposite values for new mode
+  modes: {
+    darkMode: 'lightMode',
+    lightMode: 'darkMode'
+  },
+  contentStates: {
     lightMode: {
       text:
         'As we shed light on the intricacies of psoriasis pathogenesis, we now know that other proinflammatory cytokines supplement the IL-17/23 axis and charge the immune cells in circulation.',
@@ -12,43 +18,31 @@ document.addEventListener('DOMContentLoaded', () => {
         'The complex immunopathogenic mechanisms driving psoriasis involve interactions between keratinocytes and immunecells, overloading the cycle of inflammation that keeps patients plugged into their psoriasis – bound by skin plaques causing clinical symptoms of pain, itching, and burning sensations.',
       button: 'FLIP THE SWITCH'
     }
-  };
+  },
+  toggleState() {
+    const newMode = this.modes[this.currentMode];
+    this.toggleTitleDisplayProperty(newMode);
+    this.changeTextContent({ className: '.info__content-text', newText: this.contentStates[newMode].text });
+    this.changeTextContent({ className: '.toggle-switch__text', newText: this.contentStates[newMode].button });
+    this.currentMode = newMode;
+  },
+  // set current display none && new to inline
+  toggleTitleDisplayProperty(newMode) {
+    document.querySelector(`.info__content-title--${this.currentMode}`).style.display = 'none';
+    document.querySelector(`.info__content-title--${newMode}`).style.display = 'inline-block';
+  },
+  changeTextContent({ className, newText }) {
+    document.querySelector(className).textContent = newText;
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
   const toggleSwitch = document.querySelector('.toggle-switch');
   const elementToToggle = document.querySelector('.info');
   toggleSwitch.addEventListener('click', () => {
     // returns true if it turns light mode on
     let activateLightMode = elementToToggle.classList.toggle('light-mode');
     toggleSwitch.classList.toggle('active');
-
-    if (activateLightMode) {
-      console.log('change content to light mode');
-      toggleState('lightMode', contentStates);
-    } else {
-      console.log('change content to dark mode');
-      toggleState('darkMode', contentStates);
-    }
+    toggle.toggleState();
   });
 });
-
-const toggleState = (newMode, contentStates) => {
-  // newMode === darkMode || lightMode for content obj
-  const modeClassNames =
-    newMode === 'lightMode'
-      ? { currentModeClassName: '--dark-mode', newModeClassName: '--light-mode' }
-      : { currentModeClassName: '--light-mode', newModeClassName: '--dark-mode' };
-  toggleTitleDisplayProperty(modeClassNames);
-  changeTextContent({ className: '.info__content-text', newText: contentStates[newMode].text });
-  changeTextContent({ className: '.toggle-switch__text', newText: contentStates[newMode].button });
-};
-
-const changeTextContent = ({ className, newText }) => {
-  document.querySelector(className).textContent = newText;
-};
-
-const toggleTitleDisplayProperty = ({ currentModeClassName, newModeClassName }) => {
-  console.log({ currentModeClassName, newModeClassName });
-  // set to display none
-  document.querySelector(`.info__content-title${currentModeClassName}`).style.display = 'none';
-  //   set to display inline
-  document.querySelector(`.info__content-title${newModeClassName}`).style.display = 'inline-block';
-};
