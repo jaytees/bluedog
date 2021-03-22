8b9a4d646df2e72ecb3b4740b4e7f169f7010f15
Jordan Trickett <jordan1trickett@gmail.com>
2021-03-22 13:18:09 +0000

    sorted text

--- js/index.js
+++ js/index.js
@@ -4,4 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
     toggleSwitchClass: '.toggle-switch',
     contentContainerClass: '.info'
   });
+  window.onscroll = () => {
+    var scrollY = window.pageYOffset || document.documentElement.scrollTop;
+    var navBar = document.querySelector('.nav');
+
+    scrollY <= window.lastScroll ? (navBar.style.visibility = 'visible') : (navBar.style.visibility = 'hidden');
+
+    window.lastScroll = scrollY;
+  };
 });
