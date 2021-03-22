4c6f447c20e39c3103c4c79c5b6130cf8dc1e5dd
Jordan Trickett <jordan1trickett@gmail.com>
2021-03-22 16:23:52 +0000

    added fade to info content

--- js/index.js
+++ js/index.js
@@ -5,12 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
     toggleSwitchClass: '.toggle-switch',
     contentContainerClass: '.info'
   });
+  document.querySelector('.hero__title').classList.add('hero__title--animated');
   // makes the nav bar appear when user begins to scroll up
   window.onscroll = () => {
-    var scrollY = window.pageYOffset || document.documentElement.scrollTop;
-    var navBar = document.querySelector('.nav');
+    const scrollY = window.pageYOffset || document.documentElement.scrollTop;
+    const navBar = document.querySelector('.nav');
 
     scrollY <= window.lastScroll ? (navBar.style.visibility = 'visible') : (navBar.style.visibility = 'hidden');
+
+    scrollY >= '400'
+      ? document.querySelector('.info__content').classList.add('info__content--animated')
+      : document.querySelector('.info__content').classList.remove('info__content--animated');
+
+    // scrollY < '400'
+    //   ? document.querySelector('.hero__title').classList.add('hero__title--animated')
+    //   : document.querySelector('.hero__title').classList.remove('hero__title--animated');
 
     window.lastScroll = scrollY;
   };
